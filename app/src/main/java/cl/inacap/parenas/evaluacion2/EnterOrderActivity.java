ecaf96bdfb4655af5f97549f38b4124363ccd812
RbnCaro <rub.caro@hotmail.com>
2017-10-17 22:35:33 -0600

    Enter order activity changes

--- app/src/main/java/cl/inacap/parenas/evaluacion2/EnterOrderActivity.java
+++ app/src/main/java/cl/inacap/parenas/evaluacion2/EnterOrderActivity.java
@@ -2,6 +2,10 @@ package cl.inacap.parenas.evaluacion2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import cl.inacap.parenas.evaluacion2.modelo.Client;
 
 public class EnterOrderActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class EnterOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_order);
+
+        Client client = new Client();
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<Client> listaAdapter = new ArrayAdapter<Client>(this,
+                android.R.layout.simple_list_item_1, client.getClients());
+        spinner.setAdapter(listaAdapter);
     }
 }
